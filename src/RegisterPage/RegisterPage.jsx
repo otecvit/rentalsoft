7db317e7ecd6cbd1d33e4965fea6900e0cbe0d9b
Anton Isaev <otecvit@gmail.com>
2022-02-02 22:55:02 +0300

    02.02.2022

---
+++ src/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,103 @@
+import React, { useState, useEffect, Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { userActions } from '../_actions';
+
+function RegisterPage() {
+    const [user, setUser] = useState({
+        email: '',
+        password: '',
+        promocode: '',
+        showPromo: false,
+    });
+    const [submitted, setSubmitted] = useState(false);
+    const registering = useSelector(state => state.registration.registering);
+    const dispatch = useDispatch();
+
+    // reset login status
+    useEffect(() => {
+        dispatch(userActions.logout());
+    }, []);
+
+    function handleChange(e) {
+        const { name, value } = e.target;
+        setUser(user => ({ ...user, [name]: value }));
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        setSubmitted(true);
+        if (user.email && user.password) {
+            dispatch(userActions.register(user));
+        }
+    }
+
+    function handleShowPromo() {
+        setUser(user => ({ ...user, showPromo: !user.showPromo }));
+    }
+
+    return (
+        <div className="col-lg-8 offset-lg-2">
+            <h2>Sign in</h2>
+            <form name="form" onSubmit={handleSubmit}>
+                { /*
+                <div className="form-group">
+                    <label>First Name</label>
+                    <input type="text" name="firstName" value={user.firstName} onChange={handleChange} className={'form-control' + (submitted && !user.firstName ? ' is-invalid' : '')} />
+                    {submitted && !user.firstName &&
+                        <div className="invalid-feedback">First Name is required</div>
+                    }
+                </div>
+                <div className="form-group">
+                    <label>Last Name</label>
+                    <input type="text" name="lastName" value={user.lastName} onChange={handleChange} className={'form-control' + (submitted && !user.lastName ? ' is-invalid' : '')} />
+                    {submitted && !user.lastName &&
+                        <div className="invalid-feedback">Last Name is required</div>
+                    }
+                </div>
+                <div className="form-group">
+                    <label>Username</label>
+                    <input type="text" name="username" value={user.username} onChange={handleChange} className={'form-control' + (submitted && !user.username ? ' is-invalid' : '')} />
+                    {submitted && !user.username &&
+                        <div className="invalid-feedback">Username is required</div>
+                    }
+                </div>
+                */ }
+                <div className="form-group">
+                    <label>Email</label>
+                    <input type="text" name="email" value={user.email} onChange={handleChange} className={'form-control' + (submitted && !user.email ? ' is-invalid' : '')} />
+                    {submitted && !user.username &&
+                        <div className="invalid-feedback">The field cannot be empty</div>
+                    }
+                </div>
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" name="password" value={user.password} onChange={handleChange} className={'form-control' + (submitted && !user.password ? ' is-invalid' : '')} />
+                    {submitted && !user.password &&
+                        <div className="invalid-feedback">The field cannot be empty</div>
+                    }
+                </div>
+                <div className="form-group">
+                    {
+                        !user.showPromo ? <span onClick={handleShowPromo}>У меня есть промокод</span> :
+                        <Fragment>
+                            <label>Promocode</label>
+                            <input type="text" name="promocode" value={user.promocode} onChange={handleChange} className={'form-control'} />
+                        </Fragment>
+                    }
+                </div>
+                <div className="form-group">
+                    <button className="btn btn-primary">
+                        {registering && <span className="spinner-border spinner-border-sm mr-1"></span>}
+                        Register
+                    </button>
+                    <Link to="/login" className="btn btn-link">Cancel</Link>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export { RegisterPage };
