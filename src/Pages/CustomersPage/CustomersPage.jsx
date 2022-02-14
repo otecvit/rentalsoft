36fa0defd5bdcc1133266b1d55806d93a8db5ae9
Anton Isaev <otecvit@gmail.com>
2022-02-14 21:53:05 +0300

    route

--- src/Pages/CustomersPage/CustomersPage.jsx
+++ src/Pages/CustomersPage/CustomersPage.jsx
@@ -1,9 +1,38 @@
 import React, { useState, useEffect } from 'react';
+import { useHistory, Link, Route, Redirect } from "react-router-dom";
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+
 
 function CustomersPage() {
+    const history = useHistory();
+
+    const handleAddCustomer = () =>{ 
+        let path = `/customers/new`; 
+        history.push(path);
+    }
+
+    const browseCustomers = () => {
+        return (
+            <Button variant="contained" startIcon={<AddIcon />} onClick={handleAddCustomer}>
+                ADD CUSTOMER
+            </Button>
+        )
+    }
+
+    const addCustomers = () => {
+        return (
+            <div>
+                field add customers
+            </div>
+        )
+    }
 
     return (
-        <div>Customers</div>
+        <div>
+            <Route exact path="/customers" component={browseCustomers} />
+            <Route path="/customers/new" component={addCustomers} />   
+        </div>
     );
 }
 
---
+++ src/Pages/CustomersPage/EditCustomerPage.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+
+function EditCustomerPage() {
+
+    return (
+        <div>EditCustomerPage</div>
+    );
+}
+
+export { EditCustomerPage };
