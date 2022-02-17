a29c7ed949e3c88694bc3e98e498448fb297a78d
Anton Isaev <otecvit@gmail.com>
2022-02-17 22:42:16 +0300

    customer page

--- src/Pages/CustomersPage/EditCustomerPage.jsx
+++ src/Pages/CustomersPage/EditCustomerPage.jsx
@@ -10,6 +10,10 @@ import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import Slider from "@material-ui/core/Slider";
 import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import RemoveIcon from '@mui/icons-material/Remove';
 
 const defaultValues = {
     name: "",
@@ -17,11 +21,24 @@ const defaultValues = {
     gender: "",
     os: "",
     favoriteNumber: 0,
+    phones: [
+      {
+        number: "+375295188290",
+        type: "Mobile",
+      },
+      {
+        number: "+375297096096",
+        type: "Mobile",
+      },
+    ],
   };
 
 function EditCustomerPage() {
 
     const [formValues, setFormValues] = useState(defaultValues);
+    const [selectedBtn, setSelectedBtn] = useState(1);
+    const [phonesArr, setPhone] = useState(defaultValues.phones);
+    
     const handleInputChange = (e) => {
       const { name, value } = e.target;
       setFormValues({
@@ -29,41 +46,98 @@ function EditCustomerPage() {
         [name]: value,
       });
     };
-    const handleSliderChange = (name) => (e, value) => {
+
+    const handleAddPhone = () => {
+
+      const tmpArr = {
+        phones: 
+        [
+          ...formValues.phones, 
+          {
+            number: "",
+            type: "Mobile",
+          }
+        ]
+      };
+
       setFormValues({
         ...formValues,
-        [name]: value,
+        ...tmpArr
       });
+
+
     };
+
     const handleSubmit = (event) => {
       event.preventDefault();
       console.log(formValues);
     };
+
+    console.log(formValues);
+
     return (
             <form onSubmit={handleSubmit}>
-                
-                <TextField
-                    id="name-input"
-                    name="name"
-                    label="Name"
-                    type="text"
-                    value={formValues.name}
-                    onChange={handleInputChange}
-                />
-                <TextField
-                    id="age-input"
-                    name="age"
-                    label="Age"
-                    type="number"
-                    value={formValues.age}
-                    onChange={handleInputChange}
-                />
-                <Button variant="contained" color="primary" type="submit">
-                Submit
-              </Button>
-                    
-
-                
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <ButtonGroup disableElevation variant="contained" color="primary">
+                      <Button color={selectedBtn === 1 ? "secondary" : "primary"} onClick={()=>setSelectedBtn(1)}>Individual</Button>
+                      <Button color={selectedBtn === 2 ? "secondary" : "primary"} onClick={()=>setSelectedBtn(2)}>Company</Button>
+                    </ButtonGroup>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                        id="name-input"
+                        name="name"
+                        label="Customer name"
+                        type="text"
+                        variant="outlined"
+                        size="small"
+                        value={formValues.name}
+                        onChange={handleInputChange}
+                        fullWidth 
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Grid container spacing={2}>
+                    {
+                      formValues.phones.map((phone, index) => {
+                        return (
+                          <Grid item xs={12} key={`phone${index}`}>
+                            <TextField
+                              id={`phone${index}`}
+                              name={`phone${index}`}
+                              label="Phone"
+                              type="text"
+                              size="small"
+                              variant="outlined"
+                              value={phone.number}
+                            />
+                            <IconButton>
+                                      <RemoveIcon/>
+                            </IconButton>
+                          </Grid>
+                        )                        
+                      })
+                    }
+                    <Button variant="text" onClick={handleAddPhone} size="small">+ Add phone</Button>
+                    </Grid>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                        id="age-input"
+                        name="age"
+                        label="Age"
+                        type="number"
+                        value={formValues.age}
+                        onChange={handleInputChange}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Button variant="contained" color="primary" type="submit">
+                      Submit
+                    </Button>
+                  </Grid>
+                </Grid>
             </form>
     );
 }
